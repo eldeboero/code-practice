ac0ec791d3b4d127aec2555dfcc17e83f9dbafdd
eldeboero <46005586+eldeboero@users.noreply.github.com>
2020-03-14 08:59:01 -0700

    20200314

--- W3-ES6-Fundamentals-I/2.js
+++ W3-ES6-Fundamentals-I/2.js
@@ -40,25 +40,52 @@ https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundament
 
 
 *****************************************************************************************************
-
-This should be clear that we using an arrow function to to the thing.
-  const copy_to_Clipboard = str =>
-
-
 Looks like the HTML is pretty irrelevant - it's just there to be a container for the element that gets created in js
 
 Practical application: When you want to provide a button to copy the contents of an element to the clipboard so the user can paste it into another application.
 This would allow you to escape the browser -----
 
+
+select()
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/select
+The HTMLInputElement.select() method selects all the text in a <textarea> element or in an <input> element that includes a text field.
+
+
+
+document.getSelection()
+https://developer.mozilla.org/en-US/docs/Web/API/Selection
+A Selection object represents the range of text selected by the user or the current position of the caret
+
+
+rangeCount
+https://developer.mozilla.org/en-US/docs/Web/API/Selection/rangeCount
+The Selection.rangeCount read-only property returns the number of ranges in the selection.
+Before the user has clicked a freshly loaded page, the rangeCount is 0. After the user clicks on the page, rangeCount is 1, even if no selection is visible.
+
+
+getRangeAt(0)
+https://developer.mozilla.org/en-US/docs/Web/API/Selection/getRangeAt
+The Selection.getRangeAt() method returns a range object representing one of the ranges currently selected.
+
+
+document.execCommand('copy');
+https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
+https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand#Commands
+**** OBSOLETE?! Thanks, W3Schools...
+
+Rebuilding this so you can put text in an input and copy it
+Okay - this works, but uses the same obsolete execCommand method that the example did.
+
 */
+
 
 'use strict';
 
-document.getElementById('copier').addEventListener('click', copyToClipBoard);
+document.getElementById('copierBtn').addEventListener('click', copyToClipBoard);
 
 function copyToClipBoard() {
-  const newEl = document.createElement('myText');
-  newEl.setAttribute('read-only','');
-  document.body.appendChild(newEl);
-  console.log('wtf');
+  const inputElement = document.getElementById('inputText');
+  const outputElement = document.getElementById('outputText');
+  inputElement.select();
+  document.execCommand('copy');
 }
